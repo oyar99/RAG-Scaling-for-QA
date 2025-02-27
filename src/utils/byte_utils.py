4b0075d1f5ea76686fa1bb69b63311142bb3f2fd
Jhon Rayo <rayojhon99@gmail.com>
2025-02-26 21:50:25 -0500

    fix logging formatting

--- src/utils/byte_utils.py
+++ src/utils/byte_utils.py
@@ -1,5 +1,5 @@
 def format_size(size):
-    for unit in ['B', 'K', 'M', 'G']:
+    for unit in ['B', 'KB', 'MB', 'GB']:
         if size < 1024.0:
             return f"{size:.2f}{unit}"
         size /= 1024.0
