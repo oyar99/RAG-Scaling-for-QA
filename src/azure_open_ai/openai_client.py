ddc58a467c41fe5e6c8137482f4f3448876677bf
Jhon Rayo <rayojhon99@gmail.com>
2025-04-03 20:16:41 +0000

    upgrade openai api version and update requirements

--- src/azure_open_ai/openai_client.py
+++ src/azure_open_ai/openai_client.py
@@ -25,7 +25,7 @@ class OpenAIClient(metaclass=Singleton):
             azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
             api_key = os.getenv("AZURE_OPENAI_API_KEY")
             api_version = os.getenv(
-                "AZURE_OPENAI_API_VERSION") or "2024-08-01-preview"
+                "AZURE_OPENAI_API_VERSION") or "2024-12-01-preview"
 
             self._client = AzureOpenAI(
                 azure_endpoint=azure_endpoint,
